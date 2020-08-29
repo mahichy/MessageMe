c8cd357f6a782561f9ff7f7a204ddbba09ab975c
mahi <mohiddinoshim@yahoo.com>
2020-08-29 16:08:09 +0600

    Build user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Mahi_02", password: "123456")
+User.create(username: "Oshim", password: "123456")
+User.create(username: "Mohi", password: "123456")
+User.create(username: "Munir", password: "123456")
+User.create(username: "Bishal", password: "123456")
